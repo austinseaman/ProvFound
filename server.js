7a3f1105136aba3e55768aad053f88efabd12125
root <root@beef-commander.localdomain>
2020-02-02 11:25:11 -0700

    first push

---
+++ server.js
@@ -0,0 +1,9 @@
+// const express = require('express')
+// const app = express()
+// const morgan = require('morgan')
+// const mongoose = require('mongoose')
+// const expressJwt = require('express-jwt')
+
+// app.use(express.json())
+// app.use(morgan('dev'))
+

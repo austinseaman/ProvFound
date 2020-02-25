b31501e7a064a03c59d0391d8c3102b310afdd03
austinseaman <apseaman0@gmail.com>
2020-02-25 14:46:45 -0700

    working on navigation and some light css

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import DoctorList from "./components/DoctorList.js";
+// import DoctorList from "./components/DoctorList.js";
 import "./app.css";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
-import Navigation from './components/Nav'
-import Home from './components/Home'
+import Navigation from './components/Nav';
+import Home from './components/Home';
+import FindProvider from './FindProvider'
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/graphql"
@@ -16,13 +17,12 @@ function App() {
     <ApolloProvider client={client}>
       <div className="App">
         <Navigation />
-        <Home />
         <Switch>
-          {/* <Route exact path="/" component={} />
-          <Route path="/about" component={} />
-          <Route path="/services" component={} /> */}
+          <Route exact path="/" component={Home} />
+          <Route path="/search" component={FindProvider} />
+          {/* <Route path="/services" component={} /> */}
         </Switch>
-        <DoctorList />
+        {/* <DoctorList /> */}
       </div>
     </ApolloProvider>
   );
---
+++ client/src/FindProvider.js
@@ -0,0 +1,24 @@
+import React, { PureComponent } from 'react'
+
+class FindProvider extends PureComponent {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Find your next excellent healthcare provider!</h1>
+                <form action="">
+
+                </form>
+            </div>
+        )
+    }
+}
+
+export default FindProvider
